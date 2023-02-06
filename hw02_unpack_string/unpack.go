c33e49144758930e70bd93604d61889e6ecdc09d
Alexey Savin <admin@alexeysav.in>
2023-02-06 22:37:21 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+
+	previousChar := ""
+	var isChar bool
+	var w strings.Builder
+
+	for _, r := range input {
+		current := string(r)
+		if current == "" {
+			return "", nil
+		}
+		if _, err := strconv.Atoi(current); err == nil && previousChar == "" {
+			return "", ErrInvalidString
+		}
+		if i, err := strconv.Atoi(current); err == nil {
+			if !isChar {
+				return "", ErrInvalidString
+			} else {
+				w.WriteString(strings.Repeat(previousChar, i))
+				isChar = false
+			}
+		} else {
+			if previousChar == "" {
+				previousChar = current
+			} else if isChar {
+				w.WriteString(previousChar)
+			}
+			previousChar = current
+			isChar = true
+		}
+	}
+	w.WriteString(previousChar)
+	return w.String(), nil
 }
